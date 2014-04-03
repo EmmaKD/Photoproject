403eb8e89ddc91dc9da73a1a511ca3f8991d20d5
Emmanuel <edogbevi@gmail.com>
2014-04-03 15:29:33 +0000

    added photos

--- photopro.js
+++ photopro.js
@@ -3,8 +3,8 @@
 */
 
 /* this is an array of all our images (in order) */
-var images=["drain_0m.png", "drain_1km.png", "drain_2km.png", "drain_3km.png", "drain_5km.png", "drain_50m.png", "drain_100m.png", "drain_200m.png" ,
-"drain_500m.png", "drain_ed.png", "drain_nl.png"];
+var images=["image1.png", "image2.png", "image3.png", "image4.png", "image5.png", "image6.png", "image7.png", 
+"image8.png", "image9.png]";
 
 var index = 0;
 
